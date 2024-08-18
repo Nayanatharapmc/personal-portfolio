d3a0a3fa837ae646f3c532f6a17ff02cd2c5d660
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2024-08-18 22:26:20 +0530

    Initialized home, about, projects and contact pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './index.css';
+import Home from './routes/Home';
+import About from './routes/About';
+import Projects from './routes/Projects';
+import Contact from './routes/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div><h1>My Portfolio</h1>
-    </div>
+    <>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
