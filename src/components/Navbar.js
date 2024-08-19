8279d23fa3a38d12f3e2304775703ed228f66064
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2024-08-19 07:58:32 +0530

    navigation bar completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import "./NavbarStyles.css";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa"; //import hamburger icon, fa is the category name
+import  React,{useState} from 'react';
+
+const Navbar = () => {
+  const [click, setClick] = React.useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+            <Link to="/home">Home</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
+        <li>
+            <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+            <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}> 
+        {click ? (<FaTimes size={20} style={{color:"#fff"}} />):
+        (<FaBars size={20} style={{color:"#fff"}} />)}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
