61dcd19e351a879898b53fde6a7d5cd2a324e4ec
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2024-08-20 04:34:48 +0530

    Hero sections of the pages done

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Heroimg2 from '../components/Heroimg2';
 
 const Contact = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Heroimg2 heading="CONTACT" text="Let's have a chat..."></Heroimg2>
+      <Footer />
     </div>
   )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Heroimg2 from '../components/Heroimg2';
 
 const Projects = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Heroimg2 heading="PROJECTS" text="Some of my most recent projects..."></Heroimg2>
+      <Footer />
     </div>
   )
 }
