342429780879157cac4ff45e6f1093933f5dec8f
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2024-08-21 07:02:42 +0530

    Updates

---
+++ precache-manifest.7f3341677ec90d5b3af33cdec3f7c5f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadeddc85782e08184fbfb7393b83782",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "0d0b2f0c146bdeba4561",
+    "url": "/personal-portfolio/static/css/main.9810bf84.chunk.css"
+  },
+  {
+    "revision": "765a4525e76133c52ffe",
+    "url": "/personal-portfolio/static/js/2.a3572605.chunk.js"
+  },
+  {
+    "revision": "0d0b2f0c146bdeba4561",
+    "url": "/personal-portfolio/static/js/main.55137d4c.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "d5b9b64f347681eb1756834012d1bbfe",
+    "url": "/personal-portfolio/static/media/Homepage.d5b9b64f.png"
+  },
+  {
+    "revision": "544c99633569a7fe40b4315533deb4c7",
+    "url": "/personal-portfolio/static/media/img1.544c9963.jpg"
+  }
+]);
